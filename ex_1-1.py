d87df21ea0ca1759b707ec2c8abef17ae63e0e71
Taeyoung Bae <132752021+BTae0@users.noreply.github.com>
2023-05-07 20:36:50 +0900

    Add files via upload

---
+++ ex_1-1.py
@@ -0,0 +1,15 @@
+# outStr = ""
+# count, i = 0 , 0
+# inStr = input("Type string :")
+# count = len(inStr)
+# print(count)
+
+# for i in range(0,count) :
+#     outStr += inStr[count-(i+1)]
+
+# print(f"Reversed string : {outStr}")\
+
+
+str = 'Python programming is easy!'
+
+print(str.swapcase())
